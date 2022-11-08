7550a8ea24e0b0cc1ef474c47ff9b678814767a8
Mateusz Łassa <mateuszlassa@gmail.com>
2022-11-08 10:40:17 +0100

    Karuzela zdjęć na stronie głównej

--- strona_glowna/script_strona_glowna.js
+++ strona_glowna/script_strona_glowna.js
@@ -14,4 +14,38 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    //skrypt do karuzeli zdjęć owl-carousel
+    $('.carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1.2,
+                nav: false
+            },
+            700:{
+                items: 1.7,
+                nav: false
+            },
+            800:{
+                items: 2.2,
+                nav: false
+            },
+            1000:{
+                items: 2.5,
+                nav: false
+            },
+            1200:{
+                items: 3.12,
+                nav: false
+            },
+            1700:{
+                items: 3.4,
+                nav: false
+            }
+        }
+    });
 });
