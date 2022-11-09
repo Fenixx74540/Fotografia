0231696308ff888143b8a61d18bce7d68a12e4e5
Mateusz Łassa <mateuszlassa@gmail.com>
2022-11-09 18:05:31 +0100

    Finalny wygląd strony

--- strona_glowna/script_strona_glowna.js
+++ strona_glowna/script_strona_glowna.js
@@ -5,15 +5,48 @@ $(document).ready(function(){
             //która dodaje tło do paska nawigacji
             $('.navbar').addClass("sticky");
         }else{
-            //gdy wrócimy z powrotem do góry to usuwa klasę dodającą w.w. atrybuty
+            //gdy wrócimy z powrotem do góry to usuwa klasę 
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            //po przekręceniu kółkiem 500px w osiY (pion) to doda się klasa show,
+            //która ujawnia ukrytą ikonę strzałki
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            //gey wrócimy z powrotem do góry to usuwa klasa 
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
-    // skrypt przełączający tryb menu
+
+    //skrypt przesuwania płynnie strony do góry
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    //skrypt przełączający tryb menu
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    //animacja wstukiwania liter oraz ich usuwania
+    var typed = new Typed(
+       ".typing", {
+        strings: ["Fotografem", "Blogerem", "Freelancerem"],
+        typeSpeed: 70,
+        backSpeed: 50,
+        loop: true
+       } 
+    );
+
+    var typed = new Typed(
+        ".typing-2", {
+         strings: ["Fotografem", "Blogerem", "Freelancerem"],
+         typeSpeed: 70,
+         backSpeed: 50,
+         loop: true
+        } 
+     );
 
     //skrypt do karuzeli zdjęć owl-carousel
     $('.carousel').owlCarousel({
