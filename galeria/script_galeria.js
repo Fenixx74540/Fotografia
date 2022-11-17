a05e6ab1802e244c6bd584a2a9cff0620422ffc2
Mateusz Łassa <mateuszlassa@gmail.com>
2022-11-17 20:07:21 +0100

    Poprawnie działająca galeria

--- galeria/script_galeria.js
+++ galeria/script_galeria.js
@@ -38,7 +38,7 @@ window.onload = ()=>{ //kiedy okno się otworzy
                     prevBtn.style.display = "none"; // ukrycie przycisku przewijania zdj do tyłu
                 } else {
                     preview(); // trzeba wywołać ponownie aby zaktualizować zdj, a nie pokazywało ostatnio zamkniete po kliknieciu na inne
-                    nextBtn.style.display = 'block'; // nie spowoduje to ukrycia przycisku przewijania do tyłu jeśli zaczniemy przeglądać od 1. zdj
+                    nextBtn.style.display = "block"; // nie spowoduje to ukrycia przycisku przewijania do tyłu jeśli zaczniemy przeglądać od 1. zdj
                 }
             }
             
@@ -50,22 +50,21 @@ window.onload = ()=>{ //kiedy okno się otworzy
                     nextBtn.style.display = "none"; // ukrycie przycisku przewijania zdj do przodu
                 } else {
                     preview(); // trzeba wywołać ponownie aby zaktualizować zdj, a nie pokazywało ostatnio zamkniete po kliknieciu na inne
-                    prevBtn.style.display = 'block'; // nie spowoduje to ukrycia przycisku przewijania do przodu jeśli zaczniemy przeglądać od ostatniego zdj
+                    prevBtn.style.display = "block"; // nie spowoduje to ukrycia przycisku przewijania do przodu jeśli zaczniemy przeglądać od ostatniego zdj
                 }
             }
-
-            // preview();
-            previewBox.classList.add("show"); // dodanie klasy show odpowiedzialnej za podgląd zdjęcia w oknie
-            shadow.style.display("block"); // wyświetlenie cienia w tle na całej szerokości podczas podglądu 
+            
+            
             document.querySelector("body").style.overflow = "hidden"; // brak możliwości scrollowania strony podczas podglądu
-
+            previewBox.classList.add("show"); // dodanie klasy show odpowiedzialnej za podgląd zdjęcia w oknie
+            shadow.style.display = ("block"); // wyświetlenie cienia w tle na całej szerokości podczas podglądu 
             // akcja naciśnięcia "x" podczas podglądu / zamknięcie okna
             closeIcon.onclick = ()=> {
                 newIndex = clickedImgIndex; // powoduje wyświetlenie ponownego podglądu klikniętego zdj, a nie tego co ostatnio było zamknięte
-                prevBtn.style.display = 'block'; // block powodu nie znikanie przycisku przewijania po dojechaniu do początku kolejki
-                nextBtn.style.display = 'block';  // block powodu nie znikanie przycisku przewijania po dojechaniu do końca kolejki
+                prevBtn.style.display = "block"; // block powodu nie znikanie przycisku przewijania po dojechaniu do początku kolejki
+                nextBtn.style.display = "block";  // block powodu nie znikanie przycisku przewijania po dojechaniu do końca kolejki
                 previewBox.classList.remove("show"); // ukrycie/usunięcie klasy show odpowiedzialnej za pokazywanie podglądu
-                shadow.style.display("none"); // nie wyświetlanie cienia 
+                shadow.style.display = ("none"); // nie wyświetlanie cienia 
                 document.querySelector("body").style.overflow = "scroll";
             }
         }
