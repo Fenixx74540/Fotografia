b44454d82b0ca1e76b9f1376e180e9a47e166de8
Mateusz Łassa <mateuszlassa@gmail.com>
2023-09-11 11:37:04 +0200

    Nieistotne zmiany

--- sesje/logowanie/index.php
+++ sesje/logowanie/index.php
@@ -1,21 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MariaKapsiak - logowanie</title>
-</head>
-<BODY>
-    Logowanie do konta fotografa
-    <form method="post" action="weryfikuj1.php">
-        Login:<input type="text" name="user" maxlength="20" size="20"><br>
-        Hasło:<input type="password" name="pass" maxlength="20" size="20"><br>
-        <input type="submit" value="Send"/>
-    </form>
-    
-    <a href = "https://photographerolaa.pl"> Przejdź do strony głównej </a>
-</BODY>
+<?php
+session_start(); // zapewnia dostęp do zmienny sesyjnych w danym pliku
+print $_SESSION['username'];
+?>
 
-</body>
-</html>
+
+
+<?php
+
+session_start(); // zapewnia dostęp do zmienny sesyjnych w danym pliku
+if (!isset($_SESSION['loggedin']))
+{
+    header('Location: index1.php');
+    exit();
+}
+
+$dbhost="localhost"; $dbuser="u781260265_maria"; $dbpassword="zaq1@WSX"; $dbname="u781260265_fotografia";
+$connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+if (!$connection)
+{
+    echo " MySQL Connection error." . PHP_EOL;
+    echo "Errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+mysqli_close($connection);
+?>
+<a href = "logout.php"> Logout </a>
